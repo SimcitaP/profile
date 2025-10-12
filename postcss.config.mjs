fd9f64d978182865b26cd9a3511e629683010643
Simcita <mlisanasimamnkele10@gmail.com>
2025-10-12 02:23:00 +0200

    copied important files

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,35 @@
+/*
+
+import { Config } from "postcss-load-config";
+
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: ["@tailwindcss/postcss", autoprefixer],
 };
 
 export default config;
+*/
+/*
+
+/** @type {import('postcss-load-config').Config} */
+/*import postcss from 'postcss';
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+
+const config = {
+  plugins: [
+    postcss.plugin('tailwindcss', tailwindcss),
+    postcss.plugin('autoprefixer', autoprefixer),
+  ],
+};
+/*
+   // tailwindcss: {},
+   ["@tailwindcss/postcss"],
+   // autoprefixer: {},
+  
+};
+*/
+const config = {
+  plugins: ["@tailwindcss/postcss"],
+};
+export default config;
+

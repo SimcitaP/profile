ef91f519892861fbb592075c60caa272f6edde5e
Simcita <mlisanasimamnkele10@gmail.com>
2025-11-21 00:38:48 +0200

    Responsive Rendering implement

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import {motion} from 'framer-motion';
-import useIsMobile from "./hooks/useIsMobile";
+import useIsMobile from "../hooks/useIsMobile";
 
 import { styles } from '../styles';
 import { ComputersCanvas } from './canvas';
